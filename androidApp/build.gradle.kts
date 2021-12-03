23f9354f47026527d7d1f05aa741cc2cc0c2f3e8
Marc-Antoine Fortier <mafortier@mirego.com>
2021-12-03 15:34:47 -0500

    Setup Jetpack Compose on Android (#15)
    Using 1.1.0-beta04 as it is the first version to support kotlin 1.6.0.

--- androidApp/build.gradle.kts
+++ androidApp/build.gradle.kts
@@ -17,11 +17,23 @@ android {
             isMinifyEnabled = false
         }
     }
+    buildFeatures {
+        compose = true
+    }
+
+    composeOptions {
+        kotlinCompilerExtensionVersion = "1.1.0-beta04"
+    }
 }
 
 dependencies {
     implementation(project(":shared"))
-    implementation("com.google.android.material:material:1.4.0")
-    implementation("androidx.appcompat:appcompat:1.3.1")
-    implementation("androidx.constraintlayout:constraintlayout:2.1.0")
+
+    implementation("androidx.appcompat:appcompat:1.4.0")
+    implementation("androidx.activity:activity-compose:1.4.0")
+
+    val composeVersion = "1.1.0-beta04"
+    implementation("androidx.compose.ui:ui:$composeVersion")
+    implementation("androidx.compose.ui:ui-tooling:$composeVersion")
+    implementation("androidx.compose.material:material:$composeVersion")
 }
--- androidApp/src/main/java/com/mirego/kmp/boilerplate/android/MainActivity.kt
+++ androidApp/src/main/java/com/mirego/kmp/boilerplate/android/MainActivity.kt
@@ -3,18 +3,28 @@ package com.mirego.kmp.boilerplate.android
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.mirego.kmp.boilerplate.Greeting
-import android.widget.TextView
+import androidx.activity.compose.setContent
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
 
-fun greet(): String {
-    return Greeting().greeting()
-}
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-
-        val tv: TextView = findViewById(R.id.text_view)
-        tv.text = greet()
+        setContent {
+            Greeting(text = Greeting().greeting())
+        }
     }
+}
+
+@Composable
+fun Greeting(text: String) {
+    Text(text = text)
+}
+
+@Preview
+@Composable
+fun PreviewGreeting() {
+    Greeting("Hello, Android 31")
 }
