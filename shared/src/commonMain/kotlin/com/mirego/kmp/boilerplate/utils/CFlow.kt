9f8dbd005f4ab4bb2d9368f827d05ebb37d0d19c
Marc-Antoine Fortier <mafortier@mirego.com>
2022-06-08 14:29:22 -0400

    Make CFlow an expect/actual class

--- shared/src/commonMain/kotlin/com/mirego/kmp/boilerplate/utils/CFlow.kt
+++ shared/src/commonMain/kotlin/com/mirego/kmp/boilerplate/utils/CFlow.kt
@@ -1,33 +1,7 @@
-@file:Suppress("unused")
-
 package com.mirego.kmp.boilerplate.utils
 
-import kotlinx.coroutines.CoroutineDispatcher
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.Job
 import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.launchIn
-import kotlinx.coroutines.flow.onEach
 
-fun interface Closeable {
-    fun close()
-}
+expect class CFlow<T : Any> : Flow<T>
 
-class CFlow<T : Any> internal constructor(
-    private val origin: Flow<T>,
-    private val dispatcher: CoroutineDispatcher = Dispatchers.Main
-) : Flow<T> by origin {
-    fun watch(block: (T) -> Unit): Closeable {
-        val job = Job()
-        val scope = CoroutineScope(dispatcher + job)
-
-        onEach {
-            block(it)
-        }.launchIn(scope)
-
-        return Closeable { job.cancel() }
-    }
-}
-
-internal fun <T : Any> Flow<T>.wrap(): CFlow<T> = CFlow(this)
+expect fun <T : Any> Flow<T>.wrap(): CFlow<T>
