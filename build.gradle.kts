535fd312656b27bf943a8fe8b5e89c203f701589
Marc-Antoine Fortier <mafortier@mirego.com>
2023-06-13 13:32:34 -0400

    Migrate to version catalogs

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,10 +1,11 @@
+@Suppress("DSL_SCOPE_VIOLATION")
 plugins {
-    id("com.android.application") version "8.0.0" apply false
-    id("com.android.library") version "8.0.0" apply false
-    kotlin("android") version "1.8.20" apply false
-    kotlin("multiplatform") version "1.8.20" apply false
-    id("org.jetbrains.kotlin.plugin.serialization") version "1.8.20" apply false
-    id("org.jlleitschuh.gradle.ktlint") version "11.3.1" apply false
+    alias(libs.plugins.android.application) apply false
+    alias(libs.plugins.android.library) apply false
+    alias(libs.plugins.kotlin.android) apply false
+    alias(libs.plugins.kotlin.multiplatform) apply false
+    alias(libs.plugins.serialization) apply false
+    alias(libs.plugins.ktlint) apply false
 }
 
 tasks.register("clean", Delete::class) {
