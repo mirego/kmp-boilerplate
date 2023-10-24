1a731609154b464bfb352a865a4b204fecf4b134
Jean-Philippe Couture <jpcouture@mirego.com>
2023-10-24 16:15:26 -0400

    Add the ability to perform a dependency check scan.

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,8 +7,19 @@ plugins {
     alias(libs.plugins.kotlin.native.cocoapods) apply false
     alias(libs.plugins.serialization) apply false
     alias(libs.plugins.ktlint) apply false
+
+    alias(libs.plugins.owasp.dependencycheck)
 }
 
 tasks.register("clean", Delete::class) {
     delete(rootProject.buildDir)
+}
+
+dependencyCheck {
+    formats = listOf("HTML", "SARIF")
+    outputDirectory = "reports"
+
+    analyzers.assemblyEnabled = false
+    analyzers.experimentalEnabled = true
+    analyzers.knownExploitedEnabled = true
 }
