7ede0f69a6d652cf13095e81c5676a7bf95300c2
Olivier Pineau <opineau@mirego.com>
2023-10-25 17:58:09 -0400

    Android wip

--- shared/src/commonMain/kotlin/com/mirego/kmp/boilerplate/model/Locale.kt
+++ shared/src/commonMain/kotlin/com/mirego/kmp/boilerplate/model/Locale.kt
@@ -9,11 +9,6 @@ enum class Language {
     ENGLISH,
     FRENCH;
 
-    fun toMulesoftLang() = when (this) {
-        ENGLISH -> "en-CA"
-        FRENCH -> "fr-CA"
-    }
-
     fun toLangCode() = when (this) {
         ENGLISH -> "en"
         FRENCH -> "fr"
