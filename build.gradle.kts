52f62f99733fd501ea6b4f5d69955f73555f92c1
Jean-Philippe Couture <jpcouture@mirego.com>
2023-10-27 09:39:56 -0400

    Add the ability to perform a dependency check scan. (#37)
    * Add the ability to perform a dependency check scan.
    
    * Update build.gradle.kts
    
    Co-authored-by: Marc-Antoine Fortier <mafortier@mirego.com>
    
    * Sort dependencies by alphabetical order.
    
    * Set the output format to SARIF.
    
    * Update Dependency-Check to a more recent version.
    
    * Change the output format to HTML in the setup script.
    
    * Mention the new dep-check in the README file.
    
    * Install bundle-audit in the dep-check workflow.
    
    ---------
    
    Co-authored-by: Marc-Antoine Fortier <mafortier@mirego.com>

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,8 +7,21 @@ plugins {
     alias(libs.plugins.kotlin.native.cocoapods) apply false
     alias(libs.plugins.serialization) apply false
     alias(libs.plugins.ktlint) apply false
+
+    alias(libs.plugins.owasp.dependencycheck)
 }
 
 tasks.register("clean", Delete::class) {
     delete(rootProject.buildDir)
+}
+
+dependencyCheck {
+    format = "SARIF"
+    outputDirectory = "reports"
+
+    analyzers.apply {
+        assemblyEnabled = false
+        experimentalEnabled = true
+        knownExploitedEnabled = true
+    }
 }
