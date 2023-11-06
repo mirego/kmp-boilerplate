f653a79335f3af15f0357812f4517c2996f6ec6e
Olivier Pineau <opineau@mirego.com>
2023-11-06 14:19:57 -0500

    killswitch qa env

--- shared/src/commonMain/kotlin/com/mirego/kmp/boilerplate/bootstrap/AppEnvironment.kt
+++ shared/src/commonMain/kotlin/com/mirego/kmp/boilerplate/bootstrap/AppEnvironment.kt
@@ -10,23 +10,23 @@ enum class AppEnvironment(
         key = "dev",
         "https://api.mirego.com/graphql",
         iOSSpecific = PlatformSpecific(
-            killSwitchAPIKey = "Replace with your own killSwitchAPIKey",
-            killSwitchUrl = "https://killswitch.mirego.com/killswitch"
+            killSwitchAPIKey = "", // Replace with your own killSwitchAPIKey
+            killSwitchUrl = "https://mirego-killswitch-qa.herokuapp.com/killswitch"
         ),
         androidSpecific = PlatformSpecific(
-            killSwitchAPIKey = "Replace with your own killSwitchAPIKey",
-            killSwitchUrl = "https://killswitch.mirego.com/killswitch"
+            killSwitchAPIKey = "", // Replace with your own killSwitchAPIKey
+            killSwitchUrl = "https://mirego-killswitch-qa.herokuapp.com/killswitch"
         )
     ),
     PRODUCTION(
         key = "production",
         "https://api.mirego.com/graphql",
         iOSSpecific = PlatformSpecific(
-            killSwitchAPIKey = "Replace with your own killSwitchAPIKey",
+            killSwitchAPIKey = "", // Replace with your own killSwitchAPIKey
             killSwitchUrl = "https://killswitch.mirego.com/killswitch"
         ),
         androidSpecific = PlatformSpecific(
-            killSwitchAPIKey = "Replace with your own killSwitchAPIKey",
+            killSwitchAPIKey = "", // Replace with your own killSwitchAPIKey
             killSwitchUrl = "https://killswitch.mirego.com/killswitch"
         )
     )
