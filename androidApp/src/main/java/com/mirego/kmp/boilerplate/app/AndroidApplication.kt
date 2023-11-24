48f5eb97af213220543bb6c0bc06c3160869227f
Christophe Tremblay <61481239+ChristopheTremblay@users.noreply.github.com>
2023-11-24 14:50:28 -0500

    Mirego-base-setup 9 of X: App Center (#47)
    * setup appcenter
    
    * add init on Android
    
    * base setup for fastlane (#48)

--- androidApp/src/main/java/com/mirego/kmp/boilerplate/app/AndroidApplication.kt
+++ androidApp/src/main/java/com/mirego/kmp/boilerplate/app/AndroidApplication.kt
@@ -2,6 +2,10 @@ package com.mirego.kmp.boilerplate.app
 
 import android.app.Application
 import android.content.res.Configuration
+import com.microsoft.appcenter.AppCenter
+import com.microsoft.appcenter.distribute.Distribute
+import com.microsoft.appcenter.distribute.UpdateTrack
+import com.mirego.kmp.boilerplate.R
 import com.mirego.kmp.boilerplate.app.bootstrap.AndroidBootstrap
 import com.mirego.kmp.boilerplate.bootstrap.Bootstrapper
 
@@ -12,6 +16,11 @@ class AndroidApplication : Application() {
         super.onCreate()
         bootstrapper = Bootstrapper(AndroidBootstrap(this))
         bootstrapper.initDependencies()
+
+        if (getString(R.string.appcenter_app_secret).isNotBlank()) {
+            Distribute.setUpdateTrack(UpdateTrack.PRIVATE)
+            AppCenter.start(this, getString(R.string.appcenter_app_secret), Distribute::class.java)
+        }
     }
 
     override fun onConfigurationChanged(newConfig: Configuration) {
