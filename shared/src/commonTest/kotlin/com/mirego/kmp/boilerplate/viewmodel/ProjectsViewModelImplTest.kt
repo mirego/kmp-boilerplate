e682f6a1e84f461791363f760ca90e7d0409ed11
mameza <mameza@mirego.com>
2025-03-11 16:09:59 -0400

    Fix failing tests

--- shared/src/commonTest/kotlin/com/mirego/kmp/boilerplate/viewmodel/ProjectsViewModelImplTest.kt
+++ shared/src/commonTest/kotlin/com/mirego/kmp/boilerplate/viewmodel/ProjectsViewModelImplTest.kt
@@ -7,7 +7,7 @@ import com.mirego.kmp.boilerplate.usecase.projects.ProjectItemViewData
 import com.mirego.kmp.boilerplate.usecase.projects.ProjectsUseCase
 import com.mirego.kmp.boilerplate.usecase.projects.ProjectsViewData
 import com.mirego.kmp.boilerplate.utils.stateDataData
-import com.mirego.kmp.boilerplate.viewmodel.factory.ViewModelFactory
+import com.mirego.kmp.boilerplate.viewmodel.navigation.NavigationManager
 import com.mirego.kmp.boilerplate.viewmodel.projects.ProjectsContentSection
 import com.mirego.kmp.boilerplate.viewmodel.projects.ProjectsRoot
 import com.mirego.kmp.boilerplate.viewmodel.projects.ProjectsViewModelImpl
@@ -20,14 +20,13 @@ import kotlinx.coroutines.test.runTest
 
 class ProjectsViewModelImplTest : BaseTest() {
     private val useCase = mockk<ProjectsUseCase>()
-    private val viewModelFactory = mockk<ViewModelFactory>()
+    private val navigationManager = mockk<NavigationManager>()
 
     private val viewModel by lazy {
         ProjectsViewModelImpl(
             useCase,
             i18N,
-            viewModelFactory,
-            testCoroutineScope
+            navigationManager
         )
     }
 
@@ -36,7 +35,7 @@ class ProjectsViewModelImplTest : BaseTest() {
         val viewData = ProjectsViewData.Empty
         every { useCase.projects() } returns flowOf(stateDataData(viewData))
 
-        val sections = (viewModel.rootContent as ProjectsRoot.Content).sections.elements
+        val sections = (viewModel.rootContent as ProjectsRoot.Content).sections
         assertTrue { sections[0] is ProjectsContentSection.Header }
         assertTrue { sections[1] is ProjectsContentSection.NoProjects }
     }
@@ -58,7 +57,7 @@ class ProjectsViewModelImplTest : BaseTest() {
         )
         every { useCase.projects() } returns flowOf(stateDataData(viewData))
 
-        val sections = (viewModel.rootContent as ProjectsRoot.Content).sections.elements
+        val sections = (viewModel.rootContent as ProjectsRoot.Content).sections
         assertTrue { sections[0] is ProjectsContentSection.Header }
         assertTrue { sections[1] is ProjectsContentSection.ProjectsList }
     }
